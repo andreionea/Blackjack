867525ef595048e2517a0919a4b888e237c7574e
andreionea <andreionea25@outlook.com>
2016-12-15 00:18:51 +0300

    updated main() function
    It now shuffles the deck thoroughly
    If you're wondering why the awkward interval of randomly generating a number, that's because it simulates a random break in the deck that the dealer makes when shuffling. The dealer tries to break the deck in half APPROXIMATEVELY, but not even him/herself can always split the deck perfectly in half. So I chose an interval such that the split is around half the deck. (15 - 37)
    A dealer does this about two or three times from what I've seen irl.

--- Blackjack.cpp
+++ Blackjack.cpp
@@ -1,26 +1,44 @@
 #include<iostream>
 #include<string.h>
-#include "card.h"
-#include "card_queue.h"
 #include "deck.h"
+#include<random>
 using namespace std;
 
 
 int main(int argc, char* args[])
 {
-	system("color 40");
+	system("color a");
+
 	bool quit = false;
 
-	cout << "Generating deck...";
+	cout << "Generating deck..." << endl;
 	generateDeck();
+	cout << "Deck generated!" << endl;
+
+	system("pause");
 
 	while (!quit)
 	{
+
 		system("cls");
-		cutDeck(26);
-		cutDeck(13);
-		riffleShuffle(26);
+
+		cout << "Shuffling deck..." << endl;
+
+		random_device rd;
+
+		int firstBreak = rd() % 22 + 15; cout << firstBreak << endl;
+		int secondBreak = rd() % 22 + 15; cout << secondBreak << endl;
+		int riffleBreak = rd() % 22 + 15; cout << riffleBreak << endl;
+
+		for (unsigned int count = 0; count <= 2; count++)
+		{
+			cutDeck(firstBreak);
+			cutDeck(secondBreak);
+			riffleShuffle(riffleBreak);
+		}
+
 		dealDeck();
+
 		system("pause");
 	}
 
