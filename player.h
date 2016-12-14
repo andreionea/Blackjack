823be54a6649dad3f7283b30637b198e7c87349d
andreionea <andreionea25@outlook.com>
2016-12-15 02:37:28 +0300

    added player header
    Most likely incomplete. But I'm trying to get the algorithm down first.

---
+++ player.h
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+using namespace std;
+
+struct player
+{
+	char name[30];
+	unsigned int bankroll;
+	unsigned int score;
+	unsigned int softScore;
+};
+
+void newPlayer(player P)
+{
+	cout << "New player's name: " << endl;
+	cin >> P.name;
+	cout << "How much would you like to buy-in for, " << P.name << "?" << endl;
+	cin >> P.bankroll;
+	cout << "Good luck, " << P.name << "!" << endl;
+}
+
+bool bust(player P)
+{
+	if (P.score > 21) return true;
+	return false;
+}
+
+void resetScore(player P)
+{
+	P.score = 0;
+	P.softScore = 0;
+}
+
+player table[4];
