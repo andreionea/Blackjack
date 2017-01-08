4a113f861bf7738b2f42d7d3755d3b582adbbbab
andreionea <andreionea25@outlook.com>
2017-01-08 20:31:48 +0300

    Added pocket array

--- player.h
+++ player.h
@@ -9,15 +9,19 @@ struct player
 	unsigned int softScore;
 	unsigned int splitScore;
 	unsigned int splitSoftScore;
+	card pocket[9];
 };
 
-void newPlayer(player P)
+void newPlayer(player &P)
 {
 	cout << "New player's name: " << endl;
 	cin >> P.name;
 	cout << "How much would you like to buy-in for, " << P.name << "?" << endl;
 	cin >> P.bankroll;
 	cout << "Good luck, " << P.name << "!" << endl;
+
+	P.score = 0;
+	P.softScore = 0;
 }
 
 bool bust(player P)
