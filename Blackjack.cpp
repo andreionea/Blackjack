0e63f4fd09177f98fdd15f12beb4eceed13cd0f4
andreionea <andreionea25@outlook.com>
2017-01-08 20:32:56 +0300

    dealing card functionality
    will take the dealCard function out of there. it belongs in player.h i think

--- Blackjack.cpp
+++ Blackjack.cpp
@@ -5,6 +5,14 @@
 #include<random>
 using namespace std;
 
+void dealCard(player &P, unsigned int &cardIndex, unsigned int &pocketIndex)
+{
+	P.pocket[pocketIndex] = deck[cardIndex];
+	P.score = P.score + P.pocket[pocketIndex].rank;
+	pocketIndex++;
+	cardIndex++;
+}
+
 
 int main(int argc, char* args[])
 {
@@ -47,6 +55,47 @@ int main(int argc, char* args[])
 		} while (noOfPlayers < 1 || noOfPlayers > 4);
 
 		for (unsigned int i = 0; i < noOfPlayers; i++) newPlayer(table[i]);
+
+		unsigned int cardIndex = 0; 
+		unsigned int playerIndex = 0;
+
+		while (playerIndex < noOfPlayers)
+		{
+			bool stand = false;
+			bool split = false;
+			unsigned int pocketIndex = 0;
+
+			dealCard(table[playerIndex], cardIndex, pocketIndex);
+			dealCard(table[playerIndex], cardIndex, pocketIndex);
+
+			while (stand == false || bust(table[playerIndex]) == false)
+			{
+				cout << table[playerIndex].name << ", you hold: | ";
+				for (unsigned int i = 0; i < pocketIndex; i++)
+				{
+					switch (table[playerIndex].pocket[i].rank)
+					{
+					case 11: cout << "Jack" << ' ';
+						break;
+					case 12: cout << "Queen" << ' ';
+						break;
+					case 13: cout << "King" << ' ';
+						break;
+					case 1: cout << "Ace" << ' ';
+						break;
+					default: cout << table[playerIndex].pocket[i].rank << ' ';
+					}
+
+					cout << "of ";
+					cout << table[playerIndex].pocket[i].suit <<' ' <<  '|' << ' ';
+				}
+
+				cout << "(SCORE: " << table[playerIndex].score << ')' << ' ' << endl;
+				cout << "your options: | 1. Hit | 2. Stand |";
+				cout << endl;
+				system("pause");
+			}
+		}
 
 		system("pause");
 	}
